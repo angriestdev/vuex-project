6631b5aad60478778b7246c993b164376ebc7709
angriestdev <pinkomega44@gmail.com>
2021-06-24 10:55:54 +0800

    Create button

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="quiz">
-    <v-toolbar class="elevation-1" height="50px"></v-toolbar>
+    <v-row class="ma-12">
+      <h2 class="">Quizzes & Exams</h2>
+      <v-spacer></v-spacer>
+        <v-btn
+          elevation="5"
+          class="pa-6 ma-1 white--text text-capitalize"
+          color="#31B573"
+          rounded
+        >
+          <v-icon left> mdi-plus-circle-outline </v-icon>
+          Create
+        </v-btn>
+    </v-row>
   </div>
 </template>
